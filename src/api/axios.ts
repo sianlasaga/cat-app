d25dfb74b7e80a2a57174270736f0dcc23b63efb
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2024-04-25 01:13:05 +0800

    feat: add axios instance

---
+++ src/api/axios.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL as string,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default instance;
